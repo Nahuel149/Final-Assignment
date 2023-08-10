f02ac253a43be799c1c749386afb1879d364aa8f
Mohammed Al-Asfar <elasfarc@gmail.com>
2023-08-10 03:47:40 +0300

    Add #CalculatorApp(..)
    to handle the state of the calculator && dispatch actions

--- script.js
+++ script.js
@@ -213,3 +213,71 @@ function Calculator() {
     return Number(v);
   }
 }
+
+function CalculatorApp() {
+  const { operate } = Calculator();
+  const display = document.getElementById("display");
+  let state = {
+    currentInput: "",
+    operator: null,
+    firstOperand: "",
+    secondOperand: "",
+    isDot: false,
+  };
+  const ACTIONS = {
+    UPDATE_DISPLAY: "update-display",
+    APPEND_NUMBER: "append-number",
+    HANDLE_INPUT: "handle-input",
+  };
+
+  const getS = () => console.log(state);
+  function dispatch(args) {
+    state = _dispatch({ action: ACTIONS.HANDLE_INPUT, data: args });
+  }
+
+  return { dispatch, getS };
+
+  /////***********************************
+
+  // function updateDisplay() {
+  //     display.textContent = currentInput;
+  // }
+  function _dispatch({ action, data }) {
+    const { firstOperand, secondOperand, operator, currentInput, isDot } =
+      state;
+    switch (action) {
+      case ACTIONS.HANDLE_INPUT:
+        const input = data.trim();
+        return ["+", "-", "*", "/", "%"].includes(input)
+          ? firstOperand
+            ? secondOperand
+              ? {
+                  ...state,
+                  operator: input,
+                  secondOperand: "",
+                  firstOperand: operate({
+                    n1: firstOperand,
+                    n2: secondOperand,
+                    operator,
+                  }).ans.toString(),
+                }
+              : { ...state, operator: input }
+            : state //ignore
+          : isDot
+          ? state
+          : _dispatch({ action: ACTIONS.APPEND_NUMBER, data: input });
+
+      case ACTIONS.UPDATE_DISPLAY:
+        display.textContent = currentInput;
+        return { ...state };
+      case ACTIONS.APPEND_NUMBER:
+        const updatedNumber = `${
+          operator ? secondOperand.toString() : firstOperand.toString()
+        }${data}`;
+        return {
+          ...state,
+          [operator ? "secondOperand" : "firstOperand"]: updatedNumber,
+        };
+    }
+  }
+}
