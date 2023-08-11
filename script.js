32ff775825367bc9cdd50a822abdc4690136e135
Mohamed abdallahi <97371201+BROSTORNADO@users.noreply.github.com>
2023-08-11 14:56:25 +0100

    UI updates / Modulo function added /

--- script.js
+++ script.js
@@ -1,108 +1,110 @@
-const display = document.getElementById('display');
-let currentInput = "";
-let currentOperator = "";
-let firstNumber = null;
+const display = document.getElementById('display')
+let currentInput = ''
+let operations = []
 
 function updateDisplay() {
-    display.textContent = currentInput;
+  display.textContent = currentInput
 }
 
 function appendNumber(number) {
-    if (currentInput === "0" || currentInput === "Error") {
-        currentInput = number;
-    } else {
-        currentInput += number;
-    }
-    updateDisplay();
+  if (currentInput === '0' || currentInput === 'Error') {
+    currentInput = number
+  } else {
+    currentInput += number
+  }
+  updateDisplay()
 }
 
 function handleOperator(operator) {
-    if (firstNumber === null) {
-        firstNumber = parseFloat(currentInput);
-        currentInput = "";
-        currentOperator = operator;
-    }
+  operations.push(currentInput)
+  operations.push(operator)
+  currentInput = ''
 }
 
 function operate() {
-    const secondNumber = parseFloat(currentInput);
-    if (isNaN(secondNumber)) {
-        currentInput = "Error";
-    } else {
-        switch (currentOperator) {
-            case "+":
-                currentInput = (firstNumber + secondNumber).toString();
-                break;
-            case "-":
-                currentInput = (firstNumber - secondNumber).toString();
-                break;
-            case "*":
-                currentInput = (firstNumber * secondNumber).toString();
-                break;
-            case "/":
-                if (secondNumber === 0) {
-                    currentInput = "Error: Division by zero";
-                } else {
-                    currentInput = (firstNumber / secondNumber).toString();
-                }
-                break;
+  operations.push(currentInput)
+  let result = parseFloat(operations[0])
+
+  for (let i = 1; i < operations.length; i += 2) {
+    const operator = operations[i]
+    const operand = parseFloat(operations[i + 1])
+    switch (operator) {
+      case '+':
+        result += operand
+        break
+      case '-':
+        result -= operand
+        break
+      case '*':
+        result *= operand
+        break
+      case '%':
+        result %= operand
+        break
+      case '/':
+        if (operand === 0) {
+          currentInput = 'Error: Division by zero'
+          updateDisplay()
+          return
         }
+        result /= operand
+        break
     }
-    firstNumber = null;
-    currentOperator = "";
-    updateDisplay();
+  }
+
+  currentInput = result.toString()
+  operations = []
+  updateDisplay()
 }
 
 function clear() {
-    currentInput = "";
-    firstNumber = null;
-    currentOperator = "";
-    updateDisplay();
+  currentInput = ''
+  operations = []
+  updateDisplay()
 }
 
 // Event Listeners
-document.querySelectorAll(".number").forEach(button => {
-    button.addEventListener("click", () => {
-        appendNumber(button.textContent);
-    });
-});
+document.querySelectorAll('.number').forEach((button) => {
+  button.addEventListener('click', () => {
+    appendNumber(button.textContent)
+  })
+})
 
-document.querySelectorAll(".operator").forEach(button => {
-    button.addEventListener("click", () => {
-        handleOperator(button.textContent);
-    });
-});
+document.querySelectorAll('.operator').forEach((button) => {
+  button.addEventListener('click', () => {
+    handleOperator(button.textContent)
+  })
+})
 
-document.getElementById("equals").addEventListener("click", operate);
+document.getElementById('equals').addEventListener('click', operate)
 
-document.getElementById("clear").addEventListener("click", clear);
+document.getElementById('clear').addEventListener('click', clear)
 
-document.getElementById("decimal").addEventListener("click", () => {
-    if (!currentInput.includes(".")) {
-        currentInput += ".";
-        updateDisplay();
-    }
-});
+document.getElementById('decimal').addEventListener('click', () => {
+  if (!currentInput.includes('.')) {
+    currentInput += '.'
+    updateDisplay()
+  }
+})
 
-document.getElementById("backspace").addEventListener("click", () => {
-    currentInput = currentInput.slice(0, -1);
-    updateDisplay();
-});
+document.getElementById('backspace').addEventListener('click', () => {
+  currentInput = currentInput.slice(0, -1)
+  updateDisplay()
+})
 
 // Keyboard support
-
-document.addEventListener("keydown", event => {
-    const key = event.key;
-    if (!isNaN(key) || key === ".") {
-        appendNumber(key);
-    } else if (key === "+" || key === "-" || key === "*" || key === "/") {
-        handleOperator(key);
-    } else if (key === "=" || key === "Enter") {
-        operate();
-    } else if (key === "Backspace") {
-        currentInput = currentInput.slice(0, -1);
-        updateDisplay();
-    } else if (key === "Escape") {
-        clear();
-    }
-});
+document.addEventListener('keydown', (event) => {
+  const key = event.key
+  if (!isNaN(key) || key === '.') {
+    appendNumber(key)
+  } else if ('+-*/%'.includes(key)) {
+    handleOperator(key)
+  } else if (key === '=' || key === 'Enter') {
+    operate()
+  } else if (key === 'Backspace') {
+    currentInput = currentInput.slice(0, -1)
+    updateDisplay()
+  } else if (key === 'Escape') {
+    clear()
+  }
+})
